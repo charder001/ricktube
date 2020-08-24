753f05012a3b1e600a36e1619d4ba70437db0935
Charder001 <rick.chardet@hva.nl>
2020-08-24 15:12:12 +0200

    improved responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var hamburger = document.querySelector("#hamburger")
+var collapsedNav = document.querySelector("#collapsed-nav")
 
 hamburger.addEventListener("click", function(){
     console.log("bra")
-    document.querySelector("nav").classList.toggle("hide")
+    document.querySelector("#expanded-nav").classList.toggle("hide")
+    collapsedNav.classList.toggle("hide")
 })
+
+
